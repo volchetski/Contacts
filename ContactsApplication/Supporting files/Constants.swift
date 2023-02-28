b5cba053bbd8a81db4654b7a6cd0c697bf228ca4
volchetski <zhenyahd2010@gmail.com>
2023-02-28 20:34:52 +0300

    Moving files

--- ContactsApplication/Supporting files/Constants.swift
+++ ContactsApplication/Supporting files/Constants.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+public enum Constants {
+    
+    static let contactsTabBarImageName = "person.circle.fill"
+    static let favoriteContactsTabBarImageName = "star.fill"
+    static let contactsControllerTitle = "Contacts"
+    static let favoriteContactsControllerTitle = "Favorite contacts"
+    static let addContactsButtonTitle = "Upload contacts"
+    static let contactTableViewId = "contactCell"
+}
