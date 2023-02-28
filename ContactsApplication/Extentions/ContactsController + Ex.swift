b486b1a139a77703bbb8c49d1477d8c3d65ca36d
volchetski <zhenyahd2010@gmail.com>
2023-02-28 20:33:39 +0300

    Added some extentions

--- ContactsApplication/Extentions/ContactsController + Ex.swift
+++ ContactsApplication/Extentions/ContactsController + Ex.swift
@@ -5,4 +5,18 @@
 //  Created by Евгений Волчецкий on 28.02.23.
 //
 
-import Foundation
+import UIKit
+
+extension ContactsController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Constants.contactTableViewId, for: indexPath) as? ContactTableViewCell else { fatalError("FatalError from cell") }
+                
+        cell.contact = contacts[indexPath.row]
+        return cell
+    }
+}
